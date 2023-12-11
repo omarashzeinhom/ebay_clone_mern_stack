145309bca299b512a80a093de95d80ffcea4894f
unknown <omar.ashraf.zeinhom@Hotmail.com>
2023-12-11 08:08:51 +0200

    Organize categories by parent and display them in a dropdown.

--- client/src/components/CategoryList/CategoryList.tsx
+++ client/src/components/CategoryList/CategoryList.tsx
@@ -1,23 +1,48 @@
+// CategoryList.tsx
+
 import React from "react";
-import { Category } from "../../utils/searchBarConstants"; 
+import { Category } from "../../utils/searchBarConstants";
+import "./CategoryList.scss";
 
 interface CategoryListProps {
   categories: Category[];
 }
 
 const CategoryList: React.FC<CategoryListProps> = ({ categories }) => {
+  // Organize categories by parent
+  const groupedCategories: { [parent: string]: Category[] } = {};
+
+  categories.forEach((category) => {
+    // Ensure that category.parent is not undefined
+    const parent = category.parent || "Other";
+
+    if (!groupedCategories[parent]) {
+      groupedCategories[parent] = [];
+    }
+    groupedCategories[parent].push(category);
+  });
+
+  const handleCategoryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedUrl = event.target.value;
+    if (selectedUrl) {
+      window.location.href = selectedUrl;
+    }
+  };
+
   return (
-    <div>
+    <div className="app__category-List">
       <h2>Categories</h2>
-      <ul>
-        {categories.map((category) => (
-          <li key={category.name}>
-            <a href={`/category/${encodeURIComponent(category.name)}`}>
-              {category.name}
-            </a>
-          </li>
+      <select onChange={handleCategoryChange}>
+        {Object.entries(groupedCategories).map(([parent, categoryList]) => (
+          <optgroup label={parent} key={parent}>
+            {categoryList.map((category) => (
+              <option key={category.name} value={`/category/${encodeURIComponent(category.name)}`}>
+                {category.name}
+              </option>
+            ))}
+          </optgroup>
         ))}
-      </ul>
+      </select>
     </div>
   );
 };
