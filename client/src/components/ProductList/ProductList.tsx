d2bbae324b48c3477d8d0e447c26c22cac001e6b
unknown <omar.ashraf.zeinhom@Hotmail.com>
2023-12-12 08:06:26 +0200

    fixed infiniteloop error and warning

--- client/src/components/ProductList/ProductList.tsx
+++ client/src/components/ProductList/ProductList.tsx
@@ -10,19 +10,14 @@ const ProductList: React.FC<ProductListProps> = () => {
   const { categoryName } = useParams();
   const { products, fetchProducts, setCategory } = useProductContext();
   const navigate = useNavigate();
-  useEffect(() => {
-    // Update the category state when categoryName changes
-    if (categoryName !== undefined) {
-      setCategory(categoryName);
-    }
-  }, [categoryName, setCategory]);
 
   useEffect(() => {
     // Fetch products when the category changes, but only if categoryName is defined
     if (categoryName !== undefined) {
       fetchProducts();
     }
-  }, [categoryName, fetchProducts]);
+    // eslint-disable-next-line
+  }, [categoryName /*Dont Add fetchProducts Causes infiniteLoop */]);
   console.log(categoryName);
 
   const handleCategoryChange = () => {
@@ -34,45 +29,43 @@ const ProductList: React.FC<ProductListProps> = () => {
     }
   };
 
-
   const filteredProducts = categoryName
     ? products.filter((product) => product?.parent === categoryName)
     : products;
 
-    return (
-      <div className="product-list">
-        <h2 className="product-list__header">Products</h2>
-    
-        <div>
-          <button
-            className="product-list__category-button"
-            onClick={handleCategoryChange}
-          >
-            All Categories
-          </button>
-        </div>
-    
-        <ul className="product-list__product-list">
-          {filteredProducts.map((product) => (
-            <li key={product?._id} className="product-list__product-list-item">
-              <div>
-                <img
-                  className="product-list__product-list-image"
-                  src={product?.img}
-                  alt={product?.name}
-                />
-                <p>Category: {product?.parent}</p>
-                <p className="product-list__product-list-name">{product?.name}</p>
-                <p className="product-list__product-list-price">
-                  ${product?.price}
-                </p>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-};
+  return (
+    <div className="product-list">
+      <h2 className="product-list__header">Products</h2>
 
+      <div>
+        <button
+          className="product-list__category-button"
+          onClick={handleCategoryChange}
+        >
+          All Categories
+        </button>
+      </div>
+
+      <ul className="product-list__product-list">
+        {filteredProducts.map((product) => (
+          <li key={product?._id} className="product-list__product-list-item">
+            <div>
+              <img
+                className="product-list__product-list-image"
+                src={product?.img}
+                alt={product?.name}
+              />
+              <p>Category: {product?.parent}</p>
+              <p className="product-list__product-list-name">{product?.name}</p>
+              <p className="product-list__product-list-price">
+                ${product?.price}
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default ProductList;
