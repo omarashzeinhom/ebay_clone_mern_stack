327ddd6a63cb7123c5df68d01817ad9915af5f82
unknown <omar.ashraf.zeinhom@Hotmail.com>
2023-12-22 06:01:51 +0200

    Development Version 1.0

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,15 +2,14 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
-const verifyToken = require("../middleware/verifyToken");
+const verifyToken = require("../middleware/verifyToken"); 
 
 // POSTED Routes
 router.post("/login", authController.login);
 router.post("/loginb", authController.loginBusiness);
 router.post("/register", authController.register);
 router.post("/registerb", authController.registerBusiness);
-router.get("/business", verifyToken, authController.getBusiness);
-router.get("/user", verifyToken, authController.getUser);
-router.patch("/update-avatar", verifyToken, authController.updateAvatar);
+router.get("/business", verifyToken, authController.getBusiness); 
+router.get("/user", verifyToken, authController.getUser); 
 
 module.exports = router;
