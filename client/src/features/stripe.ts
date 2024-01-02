e76048132abe4ddb2ccd28eecfc81265c0032a1e
unknown <omar.ashraf.zeinhom@Hotmail.com>
2024-01-02 04:21:32 +0200

    Handled Stripe Errors And CSP for now

--- client/src/features/stripe.ts
+++ client/src/features/stripe.ts
@@ -1,10 +1,6 @@
-import { loadStripe } from "@stripe/stripe-js";
+// stripe.ts
+import { loadStripe } from '@stripe/stripe-js';
 
-const stripePromise = loadStripe(
-  process.env.REACT_APP_STRIPE_PUBLIC_KEY || ""
-).catch((error) => {
-  console.error("Error loading Stripe:", error);
-  throw error; // Rethrow the error to propagate it to the application
-});
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY || '');
 
 export default stripePromise;
