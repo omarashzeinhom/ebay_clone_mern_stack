d4eda504715f6626882702b70ea031368f699c0b
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2024-03-20 07:49:57 +0200

    Testing for Deployment

--- client/src/utilities/constants.tsx
+++ client/src/utilities/constants.tsx
@@ -2,7 +2,7 @@ import { ProfilerOnRenderCallback } from "react";
 // CHG DEVELOPMENT AND PRODUCTION
 export const API_BASE_URL = "http://localhost:5000/";
 
-const HOME_URL = "http://localhost:3000/";
+const HOME_URL = "https://ebay-clone-mern-stack.vercel.app/";
 const DEVELOP_URL = "http://localhost:3000/";
 //const PROD_URL = "https://ebay-clone-mern-stack.vercel.app/";
 
