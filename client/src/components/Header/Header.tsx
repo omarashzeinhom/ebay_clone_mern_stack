ad10387370f945dcd0633ad2bdaa0c5f88d572d7
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2024-04-13 03:32:03 +0200

    Removed Helmet Which Removed UnSafeComponent Error

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -13,63 +13,10 @@ export default function Header() {
     document.title = `${pageTitle} | eBay Clone`;
   }, [pageTitle]);
 
-
-
-  const stripeCSP = `https://js.stripe.com https://m.stripe.network https://m.stripe.com https://m.stripe.network https://fonts.gstatic.com https://m.stripe.network https://js.stripe.com https://m.stripe.network https://m.stripe.com https://b.stripecdn.com https://m.stripe.network 'sha256-/5Guo2nzv5n/w6ukZpOBZOtTJBJPSkJ6mhHpnBgm3Ls=' https://m.stripe.network https://q.stripe.com/csp-report`
+  const stripeCSP = `https://js.stripe.com https://m.stripe.network https://m.stripe.com https://m.stripe.network https://fonts.gstatic.com https://m.stripe.network https://js.stripe.com https://m.stripe.network https://m.stripe.com https://b.stripecdn.com https://m.stripe.network 'sha256-/5Guo2nzv5n/w6ukZpOBZOtTJBJPSkJ6mhHpnBgm3Ls=' https://m.stripe.network https://q.stripe.com/csp-report`;
   return (
-    <Helmet>
+    <>
       <title>{pageTitle}</title>
-      <meta
-        name="description"
-        content="Your eBay Clone App Description Goes Here"
-      />
-      <meta
-        name="keywords"
-        content="eBay, Clone, React, TypeScript, MongoDB, Express"
-      />
-      <meta name="author" content="Omar Ashraf Zeinhom | &GoEdu" />
-      <link
-        rel="canonical"
-        href="https://ebay-clone-mern-stack.vercel.app/"
-      />
-     {/*
-      {categoryData.map((category, index) => {
-        const categoryImage = category.img;
-        return (
-          <link
-           // key={index}
-            //rel="preload"
-            //href={categoryImage}
-            //type="text"
-            //as="fetch"
-            //crossOrigin="anonymous"
-          />
-        );
-      })}
-     
-     */}
-
-     {/*
-      {products.map((product, index) => {
-        const productImage = product.img;
-        return (
-          <link
-            //key={index}
-            //rel="preload"
-            //href={productImage}
-            //type="text"
-            //as="fetch"
-            //crossOrigin="anonymous"
-          />
-        );
-      })}
-     
-     */}
-      <meta
-        httpEquiv="Content-Security-Policy"
-        content={`script-src  'self' ${stripeCSP} ;
-        object-src 'none';`}
-      />
-    </Helmet>
+    </>
   );
 }
