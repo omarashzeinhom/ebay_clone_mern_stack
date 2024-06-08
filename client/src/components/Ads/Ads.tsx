27f1f9968f70b64d359cf6894cf33f43452a8e54
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2024-06-08 09:44:46 +0300

    Fixed Ads.tsx aswell

--- client/src/components/Ads/Ads.tsx
+++ client/src/components/Ads/Ads.tsx
@@ -1,15 +1,53 @@
-import React, { useMemo, useEffect } from "react";
+import React, { useMemo, useEffect, useState } from "react";
 import "./Ads.scss";
 import { AdItems } from "../../utilities/constants";
+import { createApi } from "unsplash-js";
+
+// Unsplash API client
+const unsplashApi = createApi({
+  accessKey: process.env.REACT_APP_UNSPLASH_API_AK || ''
+});
 
 const Ads: React.FC = () => {
+  const [unsplashImages, setUnsplashImages] = useState<{ [key: string]: string }>({});
+
   useEffect(() => {
-    // Preload images
-    AdItems.forEach((item) => {
-      const img = new Image();
-      img.src = item.imageUrl;
-    });
-  }, []); 
+    const preloadImages = () => {
+      AdItems.forEach((item) => {
+        const img = new Image();
+        img.src = item.imageUrl;
+      });
+    };
+
+    preloadImages();
+  }, []);
+
+  useEffect(() => {
+    const fetchUnsplashImages = async () => {
+      const imagePromises = AdItems.map(async (item) => {
+        try {
+          const result = await unsplashApi.search.getPhotos({
+            query: item.title || item.category,
+            orientation: "landscape",
+            perPage: 1,
+          });
+
+          if (result.response?.results[0]) {
+            return { [item.id]: result.response.results[0].urls.small };
+          }
+        } catch (error) {
+          console.error(`Error fetching Unsplash image for ${item.title || item.category}:`, error);
+        }
+        return { [item.id]: "" };
+      });
+
+      const images = await Promise.all(imagePromises);
+      const imagesMap = images.reduce((acc, cur) => ({ ...acc, ...cur }), {});
+      setUnsplashImages(imagesMap);
+    };
+
+    fetchUnsplashImages();
+  }, []);
 
   const AdCarouselItems = useMemo(
     () =>
@@ -21,7 +59,7 @@ const Ads: React.FC = () => {
           <a href={item?.link}>
             <img
               className="ads__featured__item__img"
-              src={item?.imageUrl}
+              src={unsplashImages[item.id] || item.imageUrl}
               alt={item?.title}
               width={800}
               height={400}
@@ -41,8 +79,9 @@ const Ads: React.FC = () => {
           </a>
         </div>
       )),
-    []
+    [unsplashImages]
   );
+
   return (
     <div className="ads">
       <div className="ads__featured">
