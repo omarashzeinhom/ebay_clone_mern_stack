ef7993b3605f791169cf72c62c6036a2e41cfbb4
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2024-06-15 23:51:51 +0300

    Added New props bids{} object to interface Product in ./src/models

--- client/src/models/product.ts
+++ client/src/models/product.ts
@@ -6,9 +6,19 @@ export interface Product {
   quantity: number;
   img: string;
   price: number;
+  biddingStartPrice: number;
+  biddingFinalPrice: number;
   category: string;
   parent: string;
   businessId: string;
+  // Array of Objects for Bid
+  bids:[
+    {
+      userVerified: true, // Todo add check for user if verified or not
+      userBiddedId: string,
+      userBid: number,
+    }
+  ]
 }
 
 
