76d5a00c6fee88e3f3352435dce40e238082d958
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2024-06-16 05:54:37 +0300

    Removed CSP for now

--- client/src/components/Navs/Header/Header.tsx
+++ client/src/components/Navs/Header/Header.tsx
@@ -11,7 +11,6 @@ export default function Header() {
 
 
 
-  const stripeCSP = `https://js.stripe.com https://m.stripe.network https://m.stripe.com https://m.stripe.network https://fonts.gstatic.com https://m.stripe.network https://js.stripe.com https://m.stripe.network https://m.stripe.com https://b.stripecdn.com https://m.stripe.network 'sha256-/5Guo2nzv5n/w6ukZpOBZOtTJBJPSkJ6mhHpnBgm3Ls=' https://m.stripe.network https://q.stripe.com/csp-report`
   return (
     <HelmetProvider>
 <Helmet>
@@ -30,11 +29,7 @@ export default function Header() {
         href="https://ebay-clone-mern-stack.vercel.app/"
       />
 
-      <meta
-       httpEquiv="Content-Security-Policy"
-        content={`script-src  'self' ${stripeCSP} ;
-        object-src 'none';`}
-      />
+     
     </Helmet>
 
     </HelmetProvider>
